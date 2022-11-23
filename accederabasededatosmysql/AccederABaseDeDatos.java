561b73085e0693c02341d231eaf74f6490a0993c
Marcos <marcosldlf@gmail.com>
2022-11-23 02:34:08 +0100

    Últimos cambios

--- accederabasededatosmysql/AccederABaseDeDatos.java
+++ accederabasededatosmysql/AccederABaseDeDatos.java
@@ -1,25 +1,24 @@
 package accederabasededatosmysql;
 
+import static java.util.Map.entry;
+
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccederABaseDeDatos {
 
     private Connection connection;
 
-    public AccederABaseDeDatos(String driver, String url, String user, String password) {
+    public AccederABaseDeDatos(String driver, String url, String user, String password)
+            throws ClassNotFoundException, SQLException {
 
-        try {
-            Class.forName(driver);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        Class.forName(driver);
 
-        try {
-            this.setConnection(DriverManager.getConnection(url, user, password));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        this.setConnection(DriverManager.getConnection(url, user, password));
+    }
 
+    public AccederABaseDeDatos() {
     }
 
     public static void main(String[] args) {
@@ -31,31 +30,66 @@ public class AccederABaseDeDatos {
         String password = "";
 
         // ! Query
-        // select * from alumnes
-        // INSERT INTO `alumnes` (`id`, `dni_nie`, `nom`, `primer_llinatge`, `segon_llinatge`) VALUES (NULL, '123456789M', 'Marcos', 'Lopez', 'de la Fuente');
         String select = "select * from alumnes";
         String insert = "INSERT INTO `alumnes` (`id`, `dni_nie`, `nom`, `primer_llinatge`, `segon_llinatge`) VALUES (NULL, '123456789M', 'Marcos', 'Lopez', 'de la Fuente');";
         String query = select;
 
-
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        AccederABaseDeDatos aBaseDeDatos = new AccederABaseDeDatos(driver, url, user, password);
+
+        AccederABaseDeDatos aBaseDeDatos = new AccederABaseDeDatos();
 
         try {
+            aBaseDeDatos = new AccederABaseDeDatos(driver, url, user, password);
 
-            if (query.split(" ")[0].equals("select")) {
-                ResultSet resultSet = aBaseDeDatos.getConnection().createStatement().executeQuery(query);
-                aBaseDeDatos.printSelect(resultSet);
+            ///////////////////////////////////////////////////////////////////////////////////
 
-            } else {
-                aBaseDeDatos.getConnection().createStatement().executeUpdate(query);
+            ResultSet resultSet = (ResultSet) aBaseDeDatos.executeQuery(query);
+            aBaseDeDatos.printSelect(resultSet);
 
-            }
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            // int rows = (int) aBaseDeDatos.executeQuery(query);
+            // System.out.println(rows);
 
             aBaseDeDatos.getConnection().close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
         }
+
+    }
+
+    public Object executeQuery(String query) throws SQLException {
+        String firstWord = query.split(" ")[0].toUpperCase();
+        Statement statement = this.getConnection().createStatement();
+
+        Object respObject = new Object();
+
+        switch (firstWord) {
+            case "SELECT":
+                respObject = statement.executeQuery(query);
+                break;
+
+            case "INSERT":
+                respObject = statement.executeUpdate(query);
+                break;
+
+            case "UPDATE":
+                respObject = statement.executeUpdate(query);
+                break;
+
+            case "DELETE":
+                respObject = statement.executeUpdate(query);
+                break;
+
+            default:
+                System.out.println("Consulta no permitida.");
+                break;
+        }
+
+        return respObject;
     }
 
     // ! Print a Select
